d747f77b95f5e6f9122b71306680d61b7a6514d1
Shiyao <liushiyao2014@gmail.com>
2017-07-27 22:28:30 -0500

    set up

---
+++ prod/albums/index.ios.js
@@ -0,0 +1,13 @@
+// Index.ios.js - place code for iOS. 
+ 
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const appName = 'albums';
+const App = () => {
+  return (
+    <Text>Some Text</Text>  
+  );
+};
+
+AppRegistry.registerComponent(appName, () =>  App);
