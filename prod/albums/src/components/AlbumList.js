332ad6eea22e6b8131d16846a351041c49d1d010
Shiyao <liushiyao2014@gmail.com>
2017-07-30 23:20:31 -0500

    Add renderfunction

--- prod/albums/src/components/AlbumList.js
+++ prod/albums/src/components/AlbumList.js
@@ -3,16 +3,28 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 //functional component. 
+// Anytime the state of component is changed, it will rerender on the mobile device. 
 class AlbumList extends Component {
+	state = { albums: [] };
+
 	componentWillMount() {
+		//setState method is the only way to update a component state. 
 		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-			.then(response => console.log(response));
+			.then(response => this.setState({
+				albums: response.data
+			}));
+	}
+
+	renderAlbums() {
+		this.state.albums.map(album => <Text>{album.title}</Text>);
 	}
 
 	render() {
+		console.log(this.state);
+
 		return (
 			<View>
-				<Text>Album List</Text>
+				{this.renderAlbums()}
 			</View>
 		);
 	}
