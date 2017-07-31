468c8be98ca408d4b8ca1521c5a380b6cf29526c
Shiyao <liushiyao2014@gmail.com>
2017-07-30 23:08:03 -0500

    use axios to retrieve images

--- prod/albums/src/components/AlbumList.js
+++ prod/albums/src/components/AlbumList.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 //functional component. 
 class AlbumList extends Component {
+	componentWillMount() {
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+			.then(response => console.log(response));
+	}
+
 	render() {
 		return (
 			<View>
