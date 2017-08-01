dfaf7af3475e20d657b1decaaea48cc7b2a8422b
Shiyao <liushiyao2014@gmail.com>
2017-07-31 23:06:45 -0500

    Add AlbumDetail

--- prod/albums/src/components/AlbumList.js
+++ prod/albums/src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 //functional component. 
 // Anytime the state of component is changed, it will rerender on the mobile device. 
@@ -16,7 +17,9 @@ class AlbumList extends Component {
 	}
 
 	renderAlbums() {
-		return this.state.albums.map(album => <Text>{album.title}</Text>);
+		return this.state.albums.map(album => 
+			<AlbumDetail key={album.title} album={album}/>
+		);
 	}
 
 	render() {
