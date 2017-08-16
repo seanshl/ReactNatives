551bc1dfc7232f0d50ada53af43a315b1097c0ff
Shiyao <liushiyao2014@gmail.com>
2017-08-15 23:38:50 -0500

    start up

--- ReactTODO/App.js
+++ ReactTODO/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/common/Header';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={{flex: 1}}>
+        <Header headerText={'React Todo'} />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -13,11 +15,3 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
