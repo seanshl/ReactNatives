626bd38ba4cfb49cc98b32b2e2f847d9ac7e4813
Shiyao <liushiyao2014@gmail.com>
2017-08-15 23:25:06 -0500

    Add ReactTODO

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './Components/common';
+import { Header } from './components/common';
 
 class App extends Component {
 	render() {
 		return (
 			<View>
-				<Header />
+				<Header headerText="Authentication"/>
 				<Text>An App!</Text>
 			</View>
 		);
