bec89cacfa59cc839fe0d5143e008ecf85c7547c
Shiyao <liushiyao2014@gmail.com>
2017-08-16 23:45:03 -0500

    configure firebase

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+	componentWillAmount() {
+	  	var config = {
+		    apiKey: 'AIzaSyBNvokmIxYU5dfse4bZpXgsvF5SWvQtEAY',
+		    authDomain: 'react-auth-500b7.firebaseapp.com',
+		    databaseURL: 'https://react-auth-500b7.firebaseio.com',
+		    projectId: 'react-auth-500b7',
+		    storageBucket: 'react-auth-500b7.appspot.com',
+		    messagingSenderId: '918943673382'
+	  	};
+
+	  	firebase.initializeApp(config);
+	}
+
 	render() {
 		return (
 			<View>
